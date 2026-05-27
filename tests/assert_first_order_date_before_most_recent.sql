4ab2d793c11a3becd6cf2b3c4cdde9b24d68b70f
kouya takahashi <k-takahashi@tver.co.jp>
2026-05-27 06:58:22 +0000

    Add

---
+++ tests/assert_first_order_date_before_most_recent.sql
@@ -0,0 +1,15 @@
+with customer_order_dates as (
+    select
+        customer_id,
+        first_order_date,
+        most_recent_order_date
+    from {{ ref('customers') }}
+)
+
+select
+    customer_id,
+    first_order_date,
+    most_recent_order_date
+from customer_order_dates
+-- 0 件でなければならない
+where first_order_date > most_recent_order_date
